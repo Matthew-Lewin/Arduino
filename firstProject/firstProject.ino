c7a919efbd20d7bdd7b5e3f852482ea33c9ec319
Matthew Lewin <contact@lewin.io>
2020-02-12 00:39:33 +0000

    Add comments to firstProject.ino

--- firstProject/firstProject.ino
+++ firstProject/firstProject.ino
@@ -1,9 +1,7 @@
 //Define some pins!
-
 #define BLUE 3
 #define GREEN 5
 #define RED 6
-
 void setup() {
 	pinMode(RED, OUTPUT);
 	pinMode(GREEN, OUTPUT);
@@ -12,12 +10,12 @@ void setup() {
 	digitalWrite(GREEN, LOW);
 	digitalWrite(BLUE, LOW);
 }
-
 // Some variables
 int redValue;
 int greenValue;
 int blueValue;
 
+// Making the color change loop into a function cut the file size in half 😎
 void changeColor(int pin1, int pin2, int val1, int val2, int wait) {
 	for (int i = 0; i < 255; i++) {
 		++val1;
@@ -27,25 +25,14 @@ void changeColor(int pin1, int pin2, int val1, int val2, int wait) {
 		delay(wait);
 	}
 }
-
 //Main loop
-
 void loop() {
 	#define delayTime 10
-
 	redValue = 255;
 	greenValue = 0;
 	blueValue = 0;
-
 	changeColor(RED, GREEN, redValue, greenValue, delayTime);
 	changeColor(GREEN, BLUE, greenValue, blueValue, delayTime);
 	changeColor(BLUE, RED, blueValue, redValue, delayTime);
 
-	// for (int i = 0; i < 255; i++) {
-	// 	++redValue;
-	// 	--greenValue;
-	// 	analogWrite(RED, redValue);
-	// 	analogWrite(GREEN, greenValue);
-	// 	delay(delayTime);
-	// }
-}
+	 }
